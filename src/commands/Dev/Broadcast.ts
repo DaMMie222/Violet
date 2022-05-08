9f6273c268d0dda0f07cd02f12278c2a1d755e51
Fantox <shubhapratimbiswas@gmail.com>
2022-05-08 14:07:55 +0530

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -29,13 +29,12 @@ export default class Command extends BaseCommand {
 			return void (await M.reply(`Please provide the Broadcast Message.`));
 		const term = joined.trim();
 		const gifs = [
-			"https://c.tenor.com/Giv4A22u3KUAAAPo/zero-two-smile.mp4",
-			"https://c.tenor.com/a_rN_4QgvgsAAAPo/02-zero-two.mp4",
-			"https://c.tenor.com/wl5QipqQBG0AAAPo/02.mp4",
-			"https://c.tenor.com/Leh2GUmqxcsAAAPo/hi.mp4",
-			"https://c.tenor.com/7Ydf6mqbZtYAAAPo/zero-two-anime.mp4",
-			"https://c.tenor.com/dqA9YN9_oOwAAAPo/zero-two.mp4",
-			"https://c.tenor.com/ZbraeF5BI7kAAAPo/anime-zero-two.mp4",
+			"https://c.tenor.com/TZ89Av8QBVkAAAPo/violet-evergarden-violet.mp4",
+			"https://c.tenor.com/jBHnKHSIdmsAAAPo/anime-violet-evergarden.mp4",
+			"https://c.tenor.com/saq1KJCnYfoAAAPo/violet-evergarden.mp4",
+			"https://c.tenor.com/6DOajPQFU4kAAAPo/anime-smile.mp4",
+			"https://c.tenor.com/VUJcHaUY0SEAAAPo/violet-evergarden-violet.mp4",
+			
 		];
 		const selected = gifs[Math.floor(Math.random() * gifs.length)];
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -46,7 +45,7 @@ export default class Command extends BaseCommand {
 			.map((jids) => (jids.includes("g.us") ? jids : null))
 			.filter((v) => v);
 		for (let i = 0; i < chats.length; i++) {
-			const text = `*📍「Zero Two Broadcast」📍*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
+			const text = `*💖「Violet Broadcast」💖*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
 			this.client.sendMessage(chats[i], { url: selected }, MessageType.video, {
 				mimetype: Mimetype.gif,
 				caption: `${text}`,
